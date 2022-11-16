e1dc80c3f8c700e14294a11808526051042ad64d
surajchaudhary786 <78081225+surajchaudhary786@users.noreply.github.com>
2022-11-16 11:47:49 +0530

    search bar added

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,6 @@ exports.form = (req, res) => {
 
 exports.post = async (req, res) => {
   try {
-
     // USING NODE SEQUELIZE -------------------------------------------------------->
 
     // User.create({
@@ -26,27 +25,22 @@ exports.post = async (req, res) => {
     //   mobile: req.body.mobile,
     // });
 
-
     // USING SQL QUERY -------------------------------------------------------------->
 
     let query = `INSERT INTO table1s(u_name,email,age,mobile) VALUES('${req.body.name}' , '${req.body.email}' , ${req.body.age} , '${req.body.mobile}')`;
     await db.query(query);
 
     res.redirect("/user/"); //if you use redirect it must be handled in api or else use render
-    
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
-
-
 };
 
-exports.ulhelp = (req,res) =>{
+exports.ulhelp = (req, res) => {
   res.render("us_sign_up_help.ejs");
 };
 
-exports.deleteUser = async(req,res) => {
-
+exports.deleteUser = async (req, res) => {
   //USING NODE?EXPRESS----------->
 
   // let userid = req.params.id;
@@ -55,22 +49,21 @@ exports.deleteUser = async(req,res) => {
   //     u_id:userid
   //   }
   // });
-  
+
   // USING SQL QUERY------------->
 
   let query = `DELETE FROM table1s WHERE u_id=${req.params.id}`;
   await db.query(query);
 
   res.redirect("/user/");
-}
+};
 
-exports.redirectUpdatePage = async(req,res) => {
+exports.redirectUpdatePage = async (req, res) => {
   // console.log(req.params.id);
-  res.render("us_update_form.ejs",{val:req.params.id});   
-}
+  res.render("us_update_form.ejs", { val: req.params.id });
+};
 
-exports.updateUser = async(req,res)=>{
-
+exports.updateUser = async (req, res) => {
   // USING NODE?EXPRESS----------->
 
   // await User.update({u_name : req.body.name ,email : req.body.email, age : req.body.age , mobile : req.body.mobile},{
@@ -85,4 +78,12 @@ exports.updateUser = async(req,res)=>{
   await db.query(query);
 
   res.redirect("/user/");
-}
+};
+
+exports.searchUser = async (req, res) => {
+  console.log(req.body.search);
+  let query = `SELECT * FROM table1s WHERE u_name LIKE "%${req.body.search}%"`;
+  const [results] = await db.query(query);
+  console.log(results);
+  res.render("us_searched_list.ejs", { project: results }); //This project will contain rows which will dynamically shown in home.ejs
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require("./../controllers/userController");
 const router = express.Router();
 
 // All Routes of user
-//These are handeled in controllers
+// These are handeled in controllers
 router.get("/", userController.view);
 router.get("/sign_up", userController.form);
 router.get("/sign_up/about_u_login", userController.ulhelp);
@@ -11,4 +11,7 @@ router.post("/sign_up", userController.post);
 router.post("/delete/:id", userController.deleteUser);       //You can use router.delete() but here through html forms you only can send GET or POST method (So handle delete using in any one of them , here using post)
 router.get("/update/:id",userController.redirectUpdatePage);
 router.post("/update/:id",userController.updateUser);
+router.post("/search",userController.searchUser);
+
+
 module.exports = router;
